2209608018e2b76bfad08f19e74c983c3c745181
mercy-mwai <mercyneemam@gmail.com>
2025-01-10 07:21:56 +0300

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import {useEffect, useState} from 'react'
-import Filter from'./components/Form';
+import Form from'./components/Form';
+import axios from 'axios';
 
+const App =()=>{
+    const [countries, setCountries]=useState([]);
+    const[filteredCountries, setFilteredCountries]=useState([]);
+    const [selectedCountry, setSelectedCountry]=useState(null);
+    const [query, setQuery]=useState("");
+}
 
-
+useEffect   (()=>{
+    axios.
+    get('https://studies.cs.helsinki.fi/restcountries/api/all');
+})
 
 
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,1 +1,18 @@
-export default Filter
+import React from 'react';
+const Form =({query, setQuery})=>{
+    const handleSearch =(e)=>{
+        setQuery(e.target.value)
+    };
+    return (
+        <div>
+            <input
+            type="text"
+            placeholder="Search for a country ..."
+            value={query}
+            onChange={handleSearch}
+            />
+        </div>
+    );
+}
+
+export default Form
